23621673cbf54b6caa357ee49d85675e193e5fad
Shaaf <shaaf.m.shahzad@gmail.com>
2023-05-15 19:32:22 -0400

    Linked login page

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
+import 'react-native-gesture-handler';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/Login';
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: '#5F7ADB' },
+  headerTitleStyle: { color: 'white' },
+  headerTintColor: 'white',
+  headerTitleAlign: 'center',
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name ='Login' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
