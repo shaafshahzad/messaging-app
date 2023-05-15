d5dbebacc57aa6cd36476131fbb1106d1cb498e1
Shaaf <shaaf.m.shahzad@gmail.com>
2023-05-15 18:26:08 -0400

    Added and linked firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBn8HVsMrszslWvqdAnyxVpCdqsN3nBQXI",
+    authDomain: "messaging-app-6332f.firebaseapp.com",
+    projectId: "messaging-app-6332f",
+    storageBucket: "messaging-app-6332f.appspot.com",
+    messagingSenderId: "279562989852",
+    appId: "1:279562989852:web:7957b696945a4a030f4ad9"
+};
