8c99b16c5f4989859c3320a2acdb01c8988fbd83
Shaaf <shaaf.m.shahzad@gmail.com>
2023-05-17 15:58:32 -0400

    Added recent text feature

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { Component, useEffect, useState } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import { ListItem, Avatar } from 'react-native-elements'
+import { db } from '../firebase';
 
 const CustomListItem = ({ id, chatName, openChat }) => {
+
+    const [chatMessages, setChatMessages] = useState([]);
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            query(collection(db, 'chats', id, 'messages'), orderBy('timestamp', 'desc')),
+            (snapshot) => setChatMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+        return unsubscribe;
+    });
+
     return (
       <ListItem onPress={() => openChat(id, chatName)} key={id} bottomDivider>
         <Avatar
@@ -16,7 +29,7 @@ const CustomListItem = ({ id, chatName, openChat }) => {
                 {chatName}
             </ListItem.Title>
             <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                This is where the recent message goes
+                {chatMessages?.[0]?.displayName}: {chatMessages?.[0]?.message}
             </ListItem.Subtitle>
         </ListItem.Content>
 
