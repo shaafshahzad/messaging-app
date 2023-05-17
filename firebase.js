c8f0f009307b80e216f1ba528f46d8c7955a70fd
Shaaf <shaaf.m.shahzad@gmail.com>
2023-05-17 01:53:33 -0400

    Linked Firebase config to database

--- firebase.js
+++ firebase.js
@@ -1,8 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBn8HVsMrszslWvqdAnyxVpCdqsN3nBQXI",
-    authDomain: "messaging-app-6332f.firebaseapp.com",
-    projectId: "messaging-app-6332f",
-    storageBucket: "messaging-app-6332f.appspot.com",
-    messagingSenderId: "279562989852",
-    appId: "1:279562989852:web:7957b696945a4a030f4ad9"
+  apiKey: 'AIzaSyBn8HVsMrszslWvqdAnyxVpCdqsN3nBQXI',
+  authDomain: 'messaging-app-6332f.firebaseapp.com',
+  projectId: 'messaging-app-6332f',
+  storageBucket: 'messaging-app-6332f.appspot.com',
+  messagingSenderId: '279562989852',
+  appId: '1:279562989852:web:7957b696945a4a030f4ad9',
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
